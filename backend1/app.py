96a9b53b909365976d8b1bc4e302b45932e9a276
Emanuel Batista <62020783+emanuel-batista@users.noreply.github.com>
2026-04-16 00:21:36 +0000

    Adiciona configuração inicial do Docker e arquivos de backend/frontend para o projeto

---
+++ backend1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "service": "backend1",
+        "description": "API de usuários",
+        "status": "ok"
+    })
+@app.route("/health")
+def health():
+    return jsonify({"status": "up"})
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ backend2/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "service": "backend2",
+        "description": "API de usuários",
+        "status": "ok"
+    })
+@app.route("/health")
+def health():
+    return jsonify({"status": "up"})
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
